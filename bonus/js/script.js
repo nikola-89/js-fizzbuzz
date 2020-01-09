b83665fdccd4a0099c2cb8ba67c9e04ce539e6b5
Nicola <nicola.ba1989@gmail.com>
2020-01-09 18:27:30 +0100

    bonus = modificato == con === https://www.w3schools.com/js/js_comparisons.asp

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,11 +1,11 @@
 for (var i = 1; i < 101; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
         document.getElementById('list_for').innerHTML += '<li class="color-fizzbuzz">' + 'FizzBuzz' + '</li>';
     }
-    else if (i % 3 == 0) {
+    else if (i % 3 === 0) {
         document.getElementById('list_for').innerHTML += '<li class="color-fizz">' + 'Fizz' + '</li>';
     }
-    else if (i % 5 == 0) {
+    else if (i % 5 === 0) {
         document.getElementById('list_for').innerHTML += '<li class="color-buzz">' + 'Buzz' + '</li>';
     }
     else {
@@ -17,13 +17,13 @@ for (var i = 1; i < 101; i++) {
 
 var x = 1;
 while (x < 101) {
-    if (x % 3 == 0 && x % 5 == 0) {
+    if (x % 3 === 0 && x % 5 === 0) {
         document.getElementById('list_while').innerHTML += '<li class="color-fizzbuzz">' + 'FizzBuzz' + '</li>';
     }
-    else if (x % 3 == 0) {
+    else if (x % 3 === 0) {
         document.getElementById('list_while').innerHTML += '<li class="color-fizz">' + 'Fizz' + '</li>';
     }
-    else if (x % 5 == 0) {
+    else if (x % 5 === 0) {
         document.getElementById('list_while').innerHTML += '<li class="color-buzz">' + 'Buzz' + '</li>';
     }
     else {
